7de5e26a81d57b714ea802a6d1e3f8d06dbfda57
frdrk00 <masterkooo@gmail.com>
2023-04-22 12:20:51 +0200

    create: Calendar Components & styles for Calendar

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import ReactCalendar from "react-calendar";
+
+interface indexProps {}
+
+const index: FC<indexProps> = ({}) => {
+  return (
+    <div>
+      <ReactCalendar
+        minDate={new Date()}
+        className="REACT-CALENDAR p-2"
+        view="month"
+        onClickDay={(date) => console.log(date)}
+        locale="en"
+      />
+    </div>
+  );
+};
+
+export default index;

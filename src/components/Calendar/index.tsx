92ba92cbbe434ebd378c62a902bba7fbc802255d
frdrk00 <masterkooo@gmail.com>
2023-04-22 12:29:31 +0200

    added: functionality onClickDay

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,18 +1,33 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import ReactCalendar from "react-calendar";
 
 interface indexProps {}
+interface DateType {
+  justDate: Date | null;
+  dateTime: Date | null;
+}
 
 const index: FC<indexProps> = ({}) => {
+  const [date, setDate] = useState<DateType>({
+    justDate: null,
+    dateTime: null,
+  });
+
   return (
-    <div>
-      <ReactCalendar
-        minDate={new Date()}
-        className="REACT-CALENDAR p-2"
-        view="month"
-        onClickDay={(date) => console.log(date)}
-        locale="en"
-      />
+    <div className="flex h-screen flex-col items-center justify-center">
+      {date.justDate ? (
+        <div className="flex gap-4">
+
+        </div>
+      ) : (
+        <ReactCalendar
+          minDate={new Date()}
+          className="REACT-CALENDAR p-2"
+          view="month"
+          onClickDay={(date) => setDate((prev) => ({ ...prev, justDate: date }))}
+          locale="en"
+        />
+      )}
     </div>
   );
 };
